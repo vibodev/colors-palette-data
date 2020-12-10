690e47bda64253ea4441922c1906cdff60775c53
Vibo Song <vibo.dev@gmail.com>
2020-12-10 18:38:28 +0800

    添加安装后编译

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 function loadPalettes () {
   const palettes: any = require.context('./palettes', false, /\.ts$/)
   const reObj: any = {}
-  // console.log(palettes)
   palettes.keys().forEach((key: string) => {
     let palette: any= palettes(key).default
     reObj[palette.name] = palette
   })
-  console.log(reObj)
   return reObj
 }
 
